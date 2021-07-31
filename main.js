415942a443c8a74cd6ead61a71ffce3426de77a8
dishan-09 <80429027+dishan-09@users.noreply.github.com>
2021-07-31 16:44:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+studentarray = [];
+function submit() {
+    var name1 = document.getElementById("student1").value;
+    var name2 = document.getElementById("student2").value;
+    var name3 = document.getElementById("student3").value;
+    var name4 = document.getElementById("student4").value;
+    studentarray.push(name1);
+    studentarray.push(name2);
+    studentarray.push(name3);
+    studentarray.push(name4);
+
+    console.log(studentarray);
+    document.getElementById("display_name").innerHTML = studentarray;
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("sort").style.display = "inline-block";
+}
+
+function sort() {
+    studentarray.sort();
+    console.log(studentarray);
+    document.getElementById("display_name").innerHTML = studentarray;
+}
+
